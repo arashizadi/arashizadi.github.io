fe644b484b3ce57e682b31d62c99b229f241fed4
Arash Izadi <arash.iz@live.com>
2022-02-09 13:44:53 -0500

    trying blazor client-side

---
+++ ArashIzadiWebsiteGHP/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ArashIzadiWebsiteGHP.Server.Models;
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace ArashIzadiWebsiteGHP.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
